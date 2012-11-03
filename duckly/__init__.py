37757a65f73967fbeab1379603bdfbc540ab03c1
Derek Nuño <derek.nuno@gmail.com>
2012-11-02 19:12:43 -0500

    Pyjade settings and some styles.

--- duckly/__init__.py
+++ duckly/__init__.py
@@ -1,7 +1,5 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-from pyjade.compiler import Compiler
-from pyramid.compat import escape
 from pyramid_beaker import session_factory_from_settings
 
 from .models import (
@@ -26,12 +24,4 @@ def main(global_config, **settings):
 
     config.scan()
 
-    Compiler.filters['plain'] = lambda x,y: x
-    Compiler.filters['css'] = (
-        lambda x,y: '<style type="text/css">{0}</style>'.format(x))
-    Compiler.filters['javascript'] = (
-        lambda x,y: '<script type="text/javascript">{0}</script>'.format(x))
-    Compiler.filters['escaped'] = lambda x,y: escape(x)
-
     return config.make_wsgi_app()
-
