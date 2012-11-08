7852fe39d801495e66e33b7f185c4a361cdf9dec
Derek Nuño <derek.nuno@gmail.com>
2012-11-08 00:27:05 -0600

    Initial form support for registration completion.

---
+++ duckly/forms.py
@@ -0,0 +1,21 @@
+"""
+
+
+"""
+
+from wtforms import (
+    Form,
+    TextField,
+    validators
+)
+
+class RegistrationForm(Form):
+    """
+
+
+    """
+
+    username = TextField('Username', [validators.Length(min = 1, max = 30),
+                                      validators.Required()])
+    display_name = TextField('Display Name')
+
